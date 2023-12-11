9e0db7992c050d9e928c2d4bf108bb2df7791454
dylank03 <dylankataria@outlook.com>
2023-12-11 13:54:11 +0000

    added articles page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import Header from './Header'
+import Articles from './Articles'
 
 function App() {
 
   return (
     <>
-
+    <Header/>
+    <Articles/>
     </>
   )
 }
---
+++ src/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const newsApi = axios.create({baseURL: 'https://ncnewshostserver.onrender.com/api'})
+
+const getArticles = ()=>{
+    return newsApi
+    .get("/articles")
+    .then((response) => {
+        return response.data;
+    });
+}
+
+export default getArticles
