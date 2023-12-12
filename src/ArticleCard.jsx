6809dae25929dceac9bf25093c3dda15a55eab90
dylank03 <dylankataria@outlook.com>
2023-12-12 09:46:49 +0000

    added links to articles

--- src/ArticleCard.jsx
+++ src/ArticleCard.jsx
@@ -1,11 +1,9 @@
-const ArticleCard = ({articles})=>{
+import { Link } from "react-router-dom"
+
+const ArticleCard = ({article})=>{
     return(
-        <ul className="articlesList">
-            {articles.map((article)=>{
-                return(<><li className="article_list_item">{article.title}</li><img className="article_img" src = {article.article_img_url}/> <p>{article.author}</p></>)})
-            }
-        </ul>
-    )
+        <><nav><Link to = {"/articles/" + article.article_id}><h2 className="article_list_item">{article.title}</h2></Link></nav><img className="article_img" src = {article.article_img_url}/> <p>{article.author}</p></>)
 }
+
 
 export default ArticleCard
