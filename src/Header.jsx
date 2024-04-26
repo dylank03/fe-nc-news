51b8c5ddc25aa1878c6c379dbc6b432d41cc47a5
dylank03 <dylankataria@outlook.com>
2024-04-26 14:07:51 +0100

    made the site more responsive and added tab name

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,6 @@ import { useContext } from "react"
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const Header = () =>{
 
@@ -16,11 +15,13 @@ const Header = () =>{
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav>
+              <div className="d-flex align-items-center responsive-login me-3 bg-white rounded w-50" ><img className="rounded me-3 ms-2 responsive-login" style = {{height: "40px", width: "40px"}} src={user.avatar_url} /><div className="responsive-login"><h5 className="mt-3 me-2">{user.username}</h5><Link to="/login"><p className="me-2">Switch User</p></Link></div></div>
                 <Link className="me-3" to="/"><h5>Home</h5></Link>
                 <Link className="me-3" to="/articles"><h5>Articles</h5></Link>
+                <Link className ="me-3 responsive-login" to = "/login"><h5>login</h5></Link>
               </Nav>
             </Navbar.Collapse>
-            <Container className="d-flex justify-content-end user-icon">{user ? <div className="d-flex align-items-center user-icon me-5 bg-white rounded" ><img className="rounded me-3 ms-2" style = {{height: "40px", width: "40px"}} src={user.avatar_url} /><div><h5 className="mt-3 me-2">{user.username}</h5><Link to="/login"><p className="me-2">Switch User</p></Link></div></div> :<Link className="user-icon me-5" to="/login"><h5>login</h5></Link>}</Container>
+            <Container className="d-flex justify-content-end user-icon">{user ? <div className="d-flex align-items-center user-icon me-5 bg-white rounded" ><img className="rounded me-3 ms-2 user-icon" style = {{height: "40px", width: "40px"}} src={user.avatar_url} /><div className="user-icon"><h5 className="mt-3 me-2">{user.username}</h5><Link to="/login"><p className="me-2">Switch User</p></Link></div></div> :<Link className="user-icon me-5" to="/login"><h5 className="text-primary">login</h5></Link>}</Container>
         </Navbar>)
 
 
